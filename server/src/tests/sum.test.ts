97b6244eda83ecde1d72287b2d5a1405aa9b1ec7
Vishal Chaudhary <vishalchaudhary8832@gmail.com>
2025-08-02 19:19:47 +0000

    init

---
+++ server/src/tests/sum.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "@jest/globals";
+import { sum } from "../sum";
+
+describe("Sum module", () => {
+  it("should give 2 + 3 result 5", () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+});
